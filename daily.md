77c728795084e4dffbf85ba36ae5269724eac7b0
Fhasghrge <1969533391@qq.com>
2020-01-21 20:20:15 +0800

    add and find

--- daily.md
+++ daily.md
@@ -27,3 +27,17 @@
 - 一般异步api都带有回调函数
 - npm 5以后不用添加`--save`自动为`package.json`添加数据
 - `package.lock.json`文件保存`node_modules`文件包依赖树，加快分析依赖；同样可以保证下载时版本的稳定性，防止版本自动更新带来的问题
+
+### day06
+
+- 函数作参数 == 回调函数
+- 开启数据库 -- `mongod`(命令行中)
+  - 连接数据库`mongo`
+  - 断开连接数据库`exit`
+  - 查看显示所有的数据库`show dbs`
+  - 查看当前操作的数据库`db`
+  - 切换到数据库（新建）`use 数据库名称`
+  - 插入数据`db.students.insertOne({"name":"jack"})
+  - 显示当前数据库的所有集合`show collections`
+  - 查询指定对象的所有数据`db.students.find()
+- mongodb中数据库（保存数组的对象），集合（数组），文档的概念（对象）
---
+++ mongdemo/add_data.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+// 连接数据库
+mongoose.connect('mongodb://localhost/itcast')
+
+// 设计文档结构
+let userSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  password: {
+    type:String,
+    required: true
+  },
+  email: {
+    type: String
+  }
+})
+
+// 把文档结构发布为模型
+let User = mongoose.model('User', userSchema)
+
+/**
+ * 新增数据
+ */
+
+// 创建文档
+let admin = new User({
+  name: 'zs',
+  password: 'shuang',
+  email: '4446646@qq.com'
+})
+
+// 持久化存储
+admin.save( (err, ret) => {
+  if(err) {
+    console.log("存储失败")
+  }else {
+    console.log("存储成功")
+  }
+})
+
+/**
+ * 查询数据
+ */
+User.find({
+  name:'zs' // 参数对象是筛选条件
+}, (err, res) => {
+  if(err) {
+    console.log('查询失败！')
+  }else {
+    console.log(res)
+  }
+})
